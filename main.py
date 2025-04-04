9cb366df6cb8cf73f2cfec4c1c4c9799e0962de5
Nilay Jain <nilayjain12@gmail.com>
2025-04-04 15:03:55 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[float] = None
+
+my_posts = [
+    {"title": "Post 1", "content": "Content of post 1", "id": 1, "published": True, "rating": 5.0},
+    {"title": "Post 2", "content": "Content of post 2", "id": 2, "unpublished": False, "rating": 4.5}
+]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+
+@app.get("/")
+def root():
+    return {"message": "Hello World This is Social Media"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    
+    post = find_post(int(id))
+    return {"data": post}
