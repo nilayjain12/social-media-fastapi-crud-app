9758c6e372518dcc498b48b2517d7b906ebe0677
Nilay Jain <nilayjain12@gmail.com>
2025-04-06 17:57:43 -0700

    added routes for delete and updating posts

--- main.py
+++ main.py
@@ -53,3 +53,19 @@ def delete_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found!")
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = 0
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            index = i
+            print("Found index", index)
+            break
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found!")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
